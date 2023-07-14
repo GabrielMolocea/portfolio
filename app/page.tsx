15aa7ae1a532cb1a1e6a46bcb0bba84391d050f8
Gabriel Molocea <gabriel.molocea@neho.ch>
2023-07-14 14:18:49 +0300

    adding the nav buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <nav className='flex justify-between'>
+        <button >Home</button>
+        <button>About me</button>
+        <button>Project</button>
+        <button>Contact me</button>
+     </nav>
     </main>
   )
 }
