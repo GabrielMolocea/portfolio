cce19a00cd244b7d4a385c23a71e909acdd76e50
Gabriel Molocea <gabriel.molocea@neho.ch>
2023-07-15 18:54:43 +0300

    nav bar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <nav className='flex justify-between'>
-        <button >Home</button>
-        <button>About me</button>
-        <button>Project</button>
-        <button>Contact me</button>
-     </nav>
+    <main className="flex min-h-screen flex-col justify-between p-2">
+      <nav className="flex justify-between sticky">
+        <div>
+          <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black ">Home</button>
+          <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">About me</button>
+          <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">Project</button>
+        </div>
+        <div>
+          <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">Contact me</button>
+        </div>
+      </nav>
     </main>
   )
 }
