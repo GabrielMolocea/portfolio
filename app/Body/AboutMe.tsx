123eb79dd7fbfa7d0cab5550876dfb9e5ce4d568
Gabriel Molocea <gabriel.molocea@neho.ch>
2023-07-16 11:18:27 +0300

    css added and about me text

--- app/Body/AboutMe.tsx
+++ app/Body/AboutMe.tsx
@@ -1,11 +1,13 @@
 export default function AboutMe() {
     return (
         <div className="flex justify-center relative top-24">
-            <span className="p-5 drop-shadow-xl">
-                pic and skills
-            </span>
-            <span className="p-5">
-                about me
+            <span className="p-5 shadow-xl max-w-sx">
+                <div className="text-4xl mb-4 ml-3">
+                    Hello, I&rsquo;m Gabriel Molocea
+                </div>
+                <div className="max-w-20">
+                    Highly motivated and accomplished self-taught Frontend Engineer with over 2 year sof commercial experience in leveraging React, Redux, Typescript, JavaScript, andREST APIs to create exceptional user interfaces. Passionate about delivering seamless and visually captivating web applications, I am seeking a challenging role to contribute my expertise and continue to excel as a front-end engineer. With a strong foundation in self-directed learning and a proven track record of success, Iam dedicated to pushing the boundaries of front-end development and crafting immersive digital experiences.
+                </div>
             </span>
         </div>
     )
--- app/Navigation/Navigation.tsx
+++ app/Navigation/Navigation.tsx
@@ -1,16 +1,19 @@
+import Image from "next/image";
+
 export default function Navigation() {
     return (
-        <main className="flex flex-col justify-between m-2">
-            <nav className="flex justify-between sticky">
-                <div>
-                    <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black ">Home</button>
-                    <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">About me</button>
-                    <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">Project</button>
+        <nav className="flex flex-col justify-between m-2 sticky top-0 shadow-sm">
+            <div className="flex justify-between ">
+                <div className="p-2">
+                    <button className="p-1 hover:rounded-full hover:transition hover:shadow-xl hover:ring ring-blue-500 duration-150 ease-in-out">Home</button>
+                    <button className="p-1 hover:rounded-full hover:transition hover:shadow-xl hover:ring ring-blue-500 duration-150 ease-in-out">About me</button>
+                    <button className="p-1 hover:rounded-full hover:transition hover:shadow-xl hover:ring ring-blue-500 duration-150 ease-in-out">Project</button>
                 </div>
+                <button className="w-8 h-8 bg-sun bg-cover bg-clip-content mt-1" />
                 <div>
-                    <button className="m-2 hover:border-b-2 hover:transition duration-150 ease-in-out border-black">Contact me</button>
+                    <button className="p-1 hover:rounded-full hover:transition hover:shadow-xl hover:ring ring-blue-500 duration-150 ease-in-out">Contact me</button>
                 </div>
-            </nav>
-        </main>
+            </div>
+        </nav>
     )
 }
