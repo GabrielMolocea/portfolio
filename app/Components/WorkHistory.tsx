b93148e0e45c445df18b094dfdec7afbf6eb162f
Gabriel Molocea <gabriel.molocea@neho.ch>
2024-01-04 14:39:25 +0100

    added new position

--- app/Components/WorkHistory.tsx
+++ app/Components/WorkHistory.tsx
@@ -8,6 +8,30 @@ export default function WorkHistory() {
                     <>
                         <Disclosure.Button className="rounded-lg bg-blue-400 px-4 py-2 text-left text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring focus-visible:ring-blue-400 focus-visible:ring-opacity-75 w-[100%]">
                             <div className='flex justify-between'>
+                                <p>Alten</p>
+                                <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-white">
+                                    <li>{'2023-2024'}</li>
+                                </ul>
+                            </div>
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="text-gray-500">
+                            <ul className='list-disc pl-2 pt-2'>
+                                <li> Architected and executed responsive website interfaces using React, optimizing user experience across a multitude of devices.</li>
+                                <li> Spearheaded the development and maintenance of robust frontend components and features using React, Redux, and TypeScript for over 2 years, driving product innovation.</li>
+                                <li> Seamlessly integrated REST APIs, bridging the gap between frontend and backend systems, enhancing overall system efficiency.</li>
+                                <li> Conducted rigorous testing using Jest and Enzyme, and debugging of frontend code, ensuring top-tier code quality and swiftly identifying and rectifying bugs.</li>
+                                <li> Fostered collaboration with cross-functional teams to deliver cohesive and successful projects, driving team synergy.</li>
+                                <li> Thrived in an Agile environment, actively participating in daily stand-ups, sprint planning, and code reviews for over 2 years, contributing to process improvement.</li>
+                            </ul>
+                        </Disclosure.Panel>
+
+                    </>)}
+            </Disclosure >
+            <Disclosure as='div' className='mt-2'>
+                {() => (
+                    <>
+                        <Disclosure.Button className="rounded-lg bg-blue-400 px-4 py-2 text-left text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring focus-visible:ring-blue-400 focus-visible:ring-opacity-75 w-[100%]">
+                            <div className='flex justify-between'>
                                 <p>Neho</p>
                                 <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-white">
                                     <li>{'2022-2023'}</li>
