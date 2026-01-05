cf5af0c8dc3ffa23442f794a1b57e7b30329d99a
Gabriel Molocea <39118451+GabrielMolocea@users.noreply.github.com>
2026-01-05 14:21:13 +0200

    update portfolio

---
+++ app/TimeLine.tsx
@@ -0,0 +1,16 @@
+import "./timeLine.css";
+
+function TimeLine() {
+	return (
+		<div className="timeLine">
+			<div className="container">
+				<div className="text-box">
+					<h2>My Journey</h2>
+					<small>2020 - 2024</small>
+					<p>This is a timeline of my personal and professional milestones.</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default TimeLine;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
-'use client'
-import { useState } from "react";
-import Info from "./Body/Info";
-import Tabs from "./Body/Tabs";
+import TimeLine from "./TimeLine";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
-
-  return (
-    <div className={`min-h-screen ${isDarkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
-      <div className="flex justify-center pt-4 ">
-        <button className="w-10 h-10 " onClick={toggleTheme}>
-          <img className={`rounded-full ring p-1 ${isDarkMode ? 'ring-black' : 'ring-blue-700'} `} src={isDarkMode ? "images/Moon.png" : "images/Sun.png"} alt="Toggle Theme" />
-        </button>
-      </div>
-      <div className={`flex flex-col items-center lg:flex-row lg:justify-center lg:self-center pt-20 ${isDarkMode ? 'dark' : ''}`}>
-        <Info isDarkMode={isDarkMode} />
-        <Tabs isDarkMode={isDarkMode} />
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div>Presentation of me</div>
+			<TimeLine />
+		</>
+	);
 }
